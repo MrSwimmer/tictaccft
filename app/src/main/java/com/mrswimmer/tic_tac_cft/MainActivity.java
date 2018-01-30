02c34b107606a4f3487cca9c2dafb21819a8b484
mrswimmer <mrswimmerlab@gmail.com>
2018-01-30 18:03:17 +0300

    release

--- app/src/main/java/com/mrswimmer/tic_tac_cft/MainActivity.java
+++ app/src/main/java/com/mrswimmer/tic_tac_cft/MainActivity.java
@@ -1,26 +1,58 @@
 package com.mrswimmer.tic_tac_cft;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     GridLayout gridLayout;
     int grid[][] = new int[3][3];
+    public static final String APP_PREFERENCES = "mysettings";
+    TextView cw, cl, cn;
+    Button restart;
+    private SharedPreferences mSettings;
+    private int countw, countl, countn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         clearGrid();
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        countw = mSettings.getInt("cw", 0);
+        countl = mSettings.getInt("cl", 0);
+        countn = mSettings.getInt("cn", 0);
+        cw = findViewById(R.id.play_win_count);
+        cl = findViewById(R.id.play_lose_count);
+        cn = findViewById(R.id.play_n_count);
+        cw.setText(countw+"");
+        cl.setText(countl+"");
+        cn.setText(countn+"");
+        restart = findViewById(R.id.play_restart);
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearGrid();
+            }
+        });
     }
     void clearGrid() {
         for(int i=0; i<3; i++) {
             for(int j=0; j<3; j++) {
                 grid[i][j]=2;
+                ImageView imageView = findViewById(getCellId(i, j));
+                imageView.setImageResource(R.color.white);
             }
         }
     }
@@ -100,11 +132,16 @@ public class MainActivity extends Activity {
                         o++;
                     }
                 }
+                if(k==3) {
+                    addRes(2);
+                    return;
+                }
                 if(k==2||o==2) {
                     for(int l=0; l<3; l++) {
                         if(grid[i][l]==2) {
                             if(o==2) {
                                 setTac(i, l);
+                                addRes(0);
                                 Log.i("code", "nolwin");
                                 return;
                             }
@@ -127,11 +164,16 @@ public class MainActivity extends Activity {
                         o++;
                     }
                 }
+                if(k==3) {
+                    addRes(2);
+                    return;
+                }
                 if(k==2||o==2) {
                     for(int l=0; l<3; l++) {
                         if(grid[l][i]==2) {
                             if(o==2) {
                                 setTac(l, i);
+                                addRes(0);
                                 Log.i("code", "nolwin");
                                 return;
                             }
@@ -153,12 +195,17 @@ public class MainActivity extends Activity {
                     o++;
                 }
             }
+            if(k==3) {
+                addRes(2);
+                return;
+            }
             if(k==2||o==2) {
                 for(int i=0; i<3; i++) {
                     if(grid[i][i]==2) {
                         //grid[i][i]=0;
                         if(o==2) {
                             setTac(i, i);
+                            addRes(0);
                             Log.i("code", "nolwin");
                             return;
                         }
@@ -178,11 +225,16 @@ public class MainActivity extends Activity {
                     o++;
                 }
             }
+            if(k==3) {
+                addRes(2);
+                return;
+            }
             if(k==2||o==2) {
                 for(int i=0; i<3; i++) {
                     if(grid[2-i][i]==2) {
                         if(o==2) {
                             setTac(2-i, i);
+                            addRes(0);
                             Log.i("code", "nolwin");
                             return;
                         }
@@ -200,14 +252,59 @@ public class MainActivity extends Activity {
                     }
                 }
             }
+            addRes(1);
         }
+    }
+    void addRes(int res) {
+        SharedPreferences.Editor editor = mSettings.edit();
+        String message = null, button = null, title = null;
+        switch (res) {
+            case 0 :
+                countl++;
+                cl.setText(countl+"");
+                editor.putInt("cl", countl);
+                title = "Поражение!";
+                message = "Пфф, не ощутил сопротивления";
+                button = "Пусти, я ему втащу";
+                break;
+            case 1 :
+                countn++;
+                cn.setText(countn+"");
+                editor.putInt("cn", countn);
+                title = "Ничья!";
+                message = "Расслабься, меня не одолеть";
+                button = "Пусти, я ему втащу";
+                break;
+            case 2:
+                countw++;
+                cw.setText(countw+"");
+                editor.putInt("cw", countw);
+                title = "Победа!";
+                message = "Что ты такое?!";
+                button = "Развалить еще раз";
+                break;
+        }
+        editor.apply();
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton(button,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                                clearGrid();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
     void setTac(int x, int y) {
         grid[x][y]=0;
         ImageView imageView = findViewById(getCellId(x, y));
         imageView.setImageResource(R.drawable.nolik);
     }
-    void showDia() {
-        
+    void showDia(int res) {
+
     }
 }
